d34246f8098248296517c6a6135ff2fb2d5e1d2c
Adam Cardenas <cevaris@gmail.com>
2014-09-25 13:41:12 -0600

    removed comments

--- src/objects/Sphere.hs
+++ src/objects/Sphere.hs
@@ -5,9 +5,6 @@ import Data.Fixed
 
 import GLUtils
 
---spherePoints:: Float -> [(Float,Float)]
---spherePoints d = [(x,y) | x <- [(-90.0)..90.0], y <- [0.0..360.0], ((mod' x d) == 0 && x < 90), (mod' y d) == 0]
-
 spherePh:: Float -> [Float]
 spherePh d = [ ph | ph <- [(-90.0)..90.0], ((mod' ph d) == 0 && ph < 90)]
 
@@ -17,12 +14,6 @@ sphereTh d = [th | th <- [0.0..360.0], (mod' th d) == 0]
 
 drawLatBand :: Float -> (Float,Float) -> IO ()
 drawLatBand d (ph, th) =  do
-    --renderPrimitive QuadStrip $ do
-
-      --glColor3f(Cos(th)*Cos(th) , Sin(ph)*Sin(ph) , Sin(th)*Sin(th));
-      --glVertex3d(Sin(th)*Cos(ph) , Sin(ph) , Cos(th)*Cos(ph));
-      --drawVertex3f th ph 0
-      --drawVertex3f th (ph+d) 0 
 
   drawVertex3f ((sin th)*(cos ph)) (sin ph) ((cos th)*(cos ph))
   drawVertex3f ((sin th)*(cos (ph+d))) (sin (ph+d)) ((cos th)*(cos (ph+d)))
@@ -47,22 +38,6 @@ drawSphere s q (x, y, z) = do
         renderPrimitive QuadStrip $do
           mapM_ (\th -> drawLatBand q (ph, th)) (sphereTh q)
         ) (spherePh q)
-
-
-      --let points = [ (x,y) | ph <- [(-90.0)..90.0], th <- [0.0..360.0], ((mod' x d) == 0 && x < 90), (mod' y d) == 0]
-      
-      --mapM_ (\(ph, th) -> drawLatBand d (ph, th)) (spherePoints d)
-
-  --postRedisplay Nothing  
-
-      --mapM_ (\(ph, th) -> do
-      --  renderPrimitive Points $ do
-      --    drawVertex3f th ph 0
-      --    drawVertex3f th (ph+d) 0
-      --  --print $ "[ph " ++ show ph ++ "] [th " ++ show th ++ "]" 
-      --  ) (spherePoints d)
-
-        
 
         
             
